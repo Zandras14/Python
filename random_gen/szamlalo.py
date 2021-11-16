577ce4ec82e7316563d06a2d208270d900c682f9
Zandras14 <andras.zachar@gmail.com>
2021-11-16 16:28:24 +0100

    szamlalo

---
+++ random_gen/szamlalo.py
@@ -0,0 +1,17 @@
+import random
+
+numbers01 = []
+
+for i in range(0,500):
+    
+    numberRand = random.randrange( 1, 500 )
+    numbers01.append( numberRand )
+
+szamlalo = 0
+hossz = (len(numbers01))
+
+for i in range(0, hossz):
+    if numbers01[i] > 255 :
+        szamlalo = szamlalo + 1
+
+print(szamlalo)
